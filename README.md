431219415a8b5b907a522f6c92ba7f79c25f40a7
dLucs <lucas_61@hotmail.es>
2022-08-01 17:39:39 +0200

    updated files for webpack

--- README.md
+++
@@ -1,2 +0,0 @@
-# Todo-List
-A Todo-List App created with JS Modules and optimized for computers and mobile devices.
--- modules/reverseDate.js
+++
@@ -1,4 +0,0 @@
-function reverseDate(date) {
-  return date.split("-").reverse().join("-");
-}
-export { reverseDate };
